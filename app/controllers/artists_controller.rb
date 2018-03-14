60d0405c509d35ef4faaff8f0f27e9c0cca43c05
Nolan Byrnes <nolanehs@gmail.com>
2018-03-14 09:01:45 +0000

    automatically backed up by learn

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,11 +1,6 @@
 class ArtistsController < ApplicationController
   def index
-      p = Preference.new
-      if p.allow_create_artists
-        @artist = Artist.new
-      else
-        redirect_to artists_path
-      end
+    @artists = Artist.all
   end
 
   def show
